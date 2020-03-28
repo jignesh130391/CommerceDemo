e148960378336d775a0faa83327da7db674a2e3d
Jignesh Patel <jignesh.p@loginextsolutions.com>
2020-03-28 14:13:56 +0530

    Project Set Up

---
+++ CommerceDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CommerceDemo
+//
+//  Created by Jignesh on 28/03/20.
+//  Copyright © 2020 Jignesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
